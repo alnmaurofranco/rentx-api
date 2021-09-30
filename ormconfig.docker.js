b58f621cd72a2e6436f158809bdc6d44e1ebed0b
alnmaurofranco <jefeson23@live.com>
2021-09-30 06:05:41 -0300

    ✨ add: docker configured

--- ormconfig.docker.js
+++ ormconfig.docker.js
@@ -1,9 +1,9 @@
 module.exports = {
   type: 'postgres',
-  url: 'postgresql://docker:docker@172.19.0.2:5432/rentxdb',
+  url: 'postgresql://docker:docker@db:5432/rentxdb',
   synchronize: true,
   logging: false,
-  entities: ['src/modules/**/**/domain/*.ts'],
+  entities: ['src/modules/**/**/*.ts'],
   migrations: ['src/database/migrations/**/*.ts'],
   cli: {
     migrationsDir: 'src/database/migrations',
