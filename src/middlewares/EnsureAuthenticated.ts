3334971cabec832cc85f882261fcd811010ff378
alnmaurofranco <jefeson23@live.com>
2021-10-13 19:35:42 -0300

    ✅ add: added middleware to protect routes and type

--- src/middlewares/EnsureAuthenticated.ts
+++ src/middlewares/EnsureAuthenticated.ts
@@ -33,6 +33,10 @@ const ensureAuthenticated = async (
       throw new AppError('User does not exists.', 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
+
     next();
   } catch (error) {
     throw new AppError('Invalid token.', 401);
