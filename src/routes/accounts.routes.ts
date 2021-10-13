45d7e6c5b1c71cd82a5ceac9c104963c2728cfd1
alnmaurofranco <jefeson23@live.com>
2021-10-13 19:33:34 -0300

    ✅ update: rename users to accounts

---
+++ src/routes/accounts.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import { configUpload } from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/EnsureAuthenticated';
+import { CreateUserController } from '../modules/accounts/useCases/CreateUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController';
+
+const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+const accountsRouter = Router();
+
+const upload = multer(configUpload.upload('./tmp/avatar'));
+
+accountsRouter.post('/', createUserController.handle);
+
+accountsRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  updateUserAvatarController.handle
+);
+
+export { accountsRouter };
