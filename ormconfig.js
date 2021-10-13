601059cc2e9318538e0ce84ba6ce74137ecce767
alnmaurofranco <jefeson23@live.com>
2021-10-13 17:07:25 -0300

    ✅ update: added express error async

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,6 @@
 module.exports = {
   type: 'postgres',
   url: 'postgresql://docker:docker@172.19.0.2:5432/rentxdb',
-  synchronize: true,
-  logging: false,
   entities: ['src/modules/**/**/domain/*.ts'],
   migrations: ['src/database/migrations/**/*.ts'],
   cli: {
