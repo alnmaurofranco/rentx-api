e8e1485f60d23f8ebc46b176ff6b874d4680e445
Alan Mauro Franco <51250855+alnmaurofranco@users.noreply.github.com>
2022-03-04 22:03:05 -0300

    ♻ refactor(Providers): providers refactor imports

--- src/infra/providers/index.ts
+++ src/infra/providers/index.ts
@@ -1,30 +1,3 @@
-import { container } from 'tsyringe';
-
-import { IDateProvider } from '@infra/providers/DateProvider/IDateProvider';
-import { DayJSDateProvider } from '@infra/providers/DateProvider/implementations/DayJSDateProvider';
-
-import { IMailProvider } from './MailProvider/IMailProvider';
-import { MailTrapProvider } from './MailProvider/implementations/MailTrapProvider';
-import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
-import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
-import { IStorageProvider } from './StorageProvider/IStorageProvider';
-
-container.registerSingleton<IDateProvider>(
-  'DayJSDateProvider',
-  DayJSDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  'MailTrapProvider',
-  new MailTrapProvider()
-);
-
-const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  diskStorage[process.env.DISK]
-);
+import './DateProvider';
+import './MailProvider';
+import './StorageProvider';
