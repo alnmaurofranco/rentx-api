0f85fbbc7fabf8669d642c78fe865b990660c598
Alan Mauro Franco <51250855+alnmaurofranco@users.noreply.github.com>
2022-03-04 22:58:40 -0300

    ♻ refactor(DateProvider): move coding and refactor

---
+++ src/infra/providers/DateProvider/index.ts
@@ -0,0 +1,9 @@
+import { container } from 'tsyringe';
+
+import { IDateProvider } from './IDateProvider';
+import { DayJSDateProvider } from './implementations/DayJSDateProvider';
+
+container.registerSingleton<IDateProvider>(
+  'DayJSDateProvider',
+  DayJSDateProvider
+);
