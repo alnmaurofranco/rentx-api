f5ec18ca135d68d75cf7ee9f83b02f123f20f564
Alan Mauro Franco <jefeson23@live.com>
2022-03-07 21:48:25 -0300

    chore(Babel): add babel settings to production

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@infra': './src/infra',
+          '@modules': './src/modules',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+  ],
+};
