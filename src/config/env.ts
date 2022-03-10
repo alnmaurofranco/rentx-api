10096097f673178c9210cf7a98cc52947197ab53
Alan Mauro Franco <jefeson23@live.com>
2022-03-10 18:45:39 -0300

    ♻ refactor(config): add env config to deploy

---
+++ src/config/env.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+const loadedEnv = dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? './.env.production' : './.env',
+});
+
+if (loadedEnv.error) {
+  throw loadedEnv.error;
+}
