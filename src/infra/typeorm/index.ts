27dd96b97bcc67751981ff8e09feff01f001e4df
Alan Mauro Franco <jefeson23@live.com>
2022-03-11 16:45:20 -0300

    ♻ refactor(TypeORM): update host

--- src/infra/typeorm/index.ts
+++ src/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = '172.29.0.1'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   const connection = createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'development' ? host : 'localhost',
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentxdb_test'
