3c4920b681c82ef05408ab28d857d00d510d9ace
Alan Mauro Franco <jefeson23@live.com>
2022-03-14 19:34:26 -0300

    chore(scripts): add new script install application

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-(yarn && docker-compose up -d db)
+(yarn && docker-compose up -d db redis)
 (yarn db:migrate:deploy)
 (cp .env.example .env && cp .env.production.example .env.production)
 (rm -rf .env.example .env.production.example)
