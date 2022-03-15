8212e008d530a89ac59c9e83136a29aff1d53e32
Alan Mauro Franco <jefeson23@live.com>
2022-03-14 23:57:42 -0300

    ✅ test(RefreshToken): refactor

--- src/modules/accounts/useCases/RefreshToken/RefreshToken.spec.ts
+++ src/modules/accounts/useCases/RefreshToken/RefreshToken.spec.ts
@@ -1,31 +1,103 @@
-import { configAuth } from '@config/auth';
+import { AppError } from '@infra/http/errors/AppError';
 import { DayJSDateProvider } from '@infra/providers/DateProvider/implementations/DayJSDateProvider';
+import { User } from '@modules/accounts/domain/User';
+import { InMemoryUsersRepository } from '@modules/accounts/repositories/in-memory/InMemoryUsersRepository';
 import { InMemoryUsersTokensRepository } from '@modules/accounts/repositories/in-memory/InMemoryUsersTokensRepository';
-import { sign } from 'jsonwebtoken';
+import { CreateUser } from '@modules/accounts/useCases/CreateUser/CreateUser';
+
+import { AuthenticateUser } from '../AuthenticateUser/AuthenticateUser';
 import { RefreshToken } from './RefreshToken';
 
+let usersRepository: InMemoryUsersRepository;
 let usersTokensRepository: InMemoryUsersTokensRepository;
 let dayJSDateProvider: DayJSDateProvider;
+let createUser: CreateUser;
+let authenticateUser: AuthenticateUser;
 let refreshToken: RefreshToken;
+let user: Pick<User, 'name' | 'email' | 'driver_license' | 'password'>;
 
 describe('Refresh Token', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
+    usersRepository = new InMemoryUsersRepository();
     usersTokensRepository = new InMemoryUsersTokensRepository();
     dayJSDateProvider = new DayJSDateProvider();
+    createUser = new CreateUser(usersRepository);
+    authenticateUser = new AuthenticateUser(
+      usersRepository,
+      usersTokensRepository,
+      dayJSDateProvider
+    );
     refreshToken = new RefreshToken(usersTokensRepository, dayJSDateProvider);
+
+    user = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      driver_license: 'A',
+      password: '123456',
+    };
+
+    await createUser.execute(user);
   });
 
   it('Should be able to create a new refresh token', async () => {
-    const token = sign({}, configAuth.secret_token, {
-      subject: '123456',
+    const authenticate = await authenticateUser.execute({
+      email: user.email,
+      password: user.password,
     });
 
-    await refreshToken.execute(token);
+    const { refresh_token } = authenticate;
+
+    const result = await refreshToken.execute(refresh_token);
+
+    expect(result).toHaveProperty('token');
+    expect(result).toHaveProperty('refresh_token');
+  });
+
+  it('Should not be able to create a new refresh token with refresh token does not exists', async () => {
+    expect(async () => {
+      const authenticate = await authenticateUser.execute({
+        email: user.email,
+        password: user.password,
+      });
+
+      const {
+        user: { id },
+        refresh_token,
+      } = authenticate;
+
+      await usersTokensRepository.deleteById(id);
+
+      await refreshToken.execute(refresh_token);
+    }).rejects.toBeInstanceOf(AppError);
+
+    expect(async () => {
+      const authenticate = await authenticateUser.execute({
+        email: user.email,
+        password: user.password,
+      });
+
+      const {
+        user: { id },
+        refresh_token,
+      } = authenticate;
+
+      await usersTokensRepository.deleteById(id);
+
+      await refreshToken.execute(refresh_token);
+    }).rejects.toEqual(
+      expect.objectContaining({
+        message: 'Refresh Token does not exists',
+        statusCode: 500,
+      })
+    );
   });
 
   it('Should not be able to create a new refresh token with token does not formatted', async () => {
     expect(async () => {
       await refreshToken.execute('invalid-token');
     }).toBeTruthy();
+    expect(async () => {
+      await refreshToken.execute('invalid-token');
+    }).rejects.toThrowError('jwt malformed');
   });
 });
