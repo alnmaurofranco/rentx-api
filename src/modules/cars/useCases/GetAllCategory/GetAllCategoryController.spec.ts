a85534bc274ad7891f340488b3b1bfcd0a3b0e0a
Alan Mauro Franco <jefeson23@live.com>
2022-03-14 23:53:13 -0300

    ✅ test(GetAllCategoryController): refactor test

--- src/modules/cars/useCases/GetAllCategory/GetAllCategoryController.spec.ts
+++ src/modules/cars/useCases/GetAllCategory/GetAllCategoryController.spec.ts
@@ -7,20 +7,29 @@ import app from '@infra/http/app';
 import createConnectionTypeORM from '@infra/typeorm';
 
 let connection: Connection;
+let headers: { Authorization: string };
+
 describe('Get All Categorys Controller', () => {
   beforeAll(async () => {
     connection = await createConnectionTypeORM();
-
     await connection.runMigrations();
 
     const id = uuid();
+    const password = await hash('admin', 8);
 
-    const passwordHashed = await hash('admin', 12);
+    await connection.query(`INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+    values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'DRIVER')
+    `);
 
-    await connection.query(`
-     INSERT INTO users (id, name, email, password, "isAdmin", driver_license, created_at)
-     VALUES ('${id}', 'Administrador', 'admin@app.rentx.dev', '${passwordHashed}', true, 'AXDBC', 'now()')
-   `);
+    const responseToken = await request(app)
+      .post('/api/sessions')
+      .send({ email: 'admin@rentx.com.br', password: 'admin' });
+
+    const { token } = responseToken.body;
+
+    headers = {
+      Authorization: `Bearer ${token}`,
+    };
   });
 
   afterAll(async () => {
@@ -29,24 +38,14 @@ describe('Get All Categorys Controller', () => {
   });
 
   it('Should be able to get all categories', async () => {
-    const responseToken = await request(app).post('/api/sessions').send({
-      email: 'admin@app.rentx.dev',
-      password: 'admin',
-    });
+    const category = {
+      name: 'Category Supertest',
+      description: 'Category Supertest description',
+    };
 
-    const { refresh_token } = responseToken.body;
+    await request(app).post('/api/categories').set(headers).send(category);
 
-    await request(app)
-      .post('/api/categories')
-      .set({
-        Authorization: `Bearer ${refresh_token}`,
-      })
-      .send({
-        name: 'Category Supertest',
-        description: 'Category Supertest description',
-      });
-
-    const response = await request(app).get('/api/categories').send();
+    const response = await request(app).get('/api/categories');
 
     expect(response.status).toBe(200);
     expect(response.body.length).toBe(1);
@@ -58,12 +57,15 @@ describe('Get All Categorys Controller', () => {
     ]);
   });
 
-  // it('Should not be able to get all categorys with empty array', async () => {
-  //   await connection.query('DELETE FROM categories');
+  it('Should not be able to get all categorys with empty array', async () => {
+    await connection.query('DELETE FROM categories');
 
-  //   const response = await request(app).get('/api/categories');
+    const response = await request(app).get('/api/categories');
 
-  //   expect(response.status).toBe(400);
-  //   expect(response.body).toHaveProperty('error');
-  // });
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty(
+      'error',
+      'There are currently no categories.'
+    );
+  });
 });
