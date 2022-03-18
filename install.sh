61017a5c622b3bb2c7e92dcd6f517b6c0e4c4440
alnmaurofranco <jefeson23@live.com>
2022-03-18 14:57:07 -0300

    🔥 remove script file

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-(yarn && docker-compose up -d db redis)
-(yarn db:migrate:deploy)
-(cp .env.example .env && cp .env.production.example .env.production)
-(rm -rf .env.example .env.production.example)
